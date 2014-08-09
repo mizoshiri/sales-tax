2fe05c5ba9ba3c3ca69bf10aff005d0aa559b05c
utwang <x2x2tone@gmail.com>
2014-08-09 12:05:35 +0900

    Add db seed for items & item types

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ItemType.delete_all
+Item.delete_all
+
+type1 = ItemType.create(name: 'books', sales_tax: false)
+type2 = ItemType.create(name: 'food', sales_tax: false)
+type3 = ItemType.create(name: 'medical products', sales_tax: false)
+type4 = ItemType.create(name: 'music products', sales_tax: true)
+type5 = ItemType.create(name: 'clothing', sales_tax: true)
+type6 = ItemType.create(name: 'furniture', sales_tax: true)
+type7 = ItemType.create(name: 'furniture', sales_tax: true)
+type8 = ItemType.create(name: 'electric appliances', sales_tax: true)
+
+Item.create(name: 'book', price: 12.49, type_id: type1.id, import: false)
+Item.create(name: 'music cd', price: 14.99, type_id: type4.id, import: false)
+Item.create(name: 'chocolate bar', price: 0.85, type_id: type2.id, import: false)
